afbd4ea15386b4b32088a554a606d7555766e3f8
Ryan Forsythe <ryan@pdxcb.net>
2018-12-09 17:22:55 -0800

    a doubly linked list that does not work

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,37 +1,79 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+
+struct Node<T> {
+    value: T,
+    prev: Link<T>,
+    next: Link<T>
+}
+
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
+
+impl<T> Node<T> {
+    fn new(value: T) -> Rc<RefCell<Self>> {
+        let s = Rc::new(RefCell::new(Node { value, prev: None, next: None}));
+        s.borrow_mut().prev = Some(s.clone());
+        s.borrow_mut().next = Some(s.clone());
+        return s;
+    }
+
+    fn append(&mut self, value: T) {
+        let newnext = Self::new(value);
+        let oldnext = self.next.take().unwrap();
+        oldnext.borrow_mut().prev = Some(newnext.clone());
+        newnext.borrow_mut().next = Some(oldnext.clone());
+        
+        let prev = self.prev.take().unwrap();
+        let this = prev.borrow_mut().next.take().unwrap();
+        newnext.borrow_mut().prev = Some(this.clone());
+    }
+
+    fn remove(&mut self) {
+        self.next.take().unwrap().borrow_mut().prev = Some(self.prev.take().unwrap().clone());
+        self.prev.take().unwrap().borrow_mut().next = Some(self.next.take().unwrap().clone());
+    }
+}
+
 fn run_game(playercount: usize, lastmarble: u32) -> u32 {
-    let mut ring: Vec<u32> = [0, 2, 1, 3].to_vec();
+    let mut curr = Node::new(0);
     let mut players: Vec<u32> = Vec::new();
     for _ in 0..playercount { players.push(0); }
-    let mut marblevalue = 3;
-    let mut currringpos: usize = 3;
-    let mut currplayer: usize = 3;
+    let mut marblevalue = 0;
+    let mut currplayer: usize = 0;
 
     loop {
         if marblevalue > lastmarble { break; }
-
+        
         marblevalue += 1;
         currplayer = (currplayer + 1) % players.len();
         
         if marblevalue % 23 == 0 {
             players[currplayer] += marblevalue;
-            currringpos = match currringpos {
-                p if p > 7 => p - 7,
-                p => ring.len() - (7 - p)
-            };
-            players[currplayer] += ring.remove(currringpos);
-            currringpos = currringpos % ring.len();
+            for _ in 0..7 {
+                let newcurr = curr.borrow_mut().prev.take().unwrap().clone();
+                curr = newcurr;
+            }
+            players[currplayer] += curr.borrow().value;
+            curr.borrow_mut().remove();
         } else {
-            currringpos = (currringpos + 2) % ring.len();
-            ring.insert(currringpos, marblevalue);
+            let newcurr = curr.borrow_mut().next.take().unwrap().clone();
+            curr = newcurr;
+            curr.borrow_mut().append(marblevalue);
         }
     }
 
     players.sort();
-    return *players.last().expect("should have had some players");
+    return *players.last().unwrap();
 }
 
 const RUNTESTS: bool = true;
 fn main() {
+    let h = Node::new(0);
+    println!("{}", h.borrow().value);
+    h.borrow_mut().append(1);
+    let newcurr = h.borrow_mut().next.take().unwrap().clone();
+    println!("{}", newcurr.borrow().value);
+/*
     if RUNTESTS {
         assert_eq!(run_game(9, 25), 32);
         assert_eq!(run_game(10, 1618), 8317);
@@ -41,4 +83,6 @@ fn main() {
         assert_eq!(run_game(30, 5807), 37305);
     }
     println!("High score (part a): {}", run_game(411, 71058));
+    println!("High score (part b): {}", run_game(411, 71058 * 100));
+    */
 }
