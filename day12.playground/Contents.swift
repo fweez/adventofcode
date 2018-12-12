77355eeca0cfaa441e9f2d619dada182eb6753ab
Ryan Forsythe <ryan@pdxcb.net>
2018-12-12 10:38:00 -0800

    stabbing towards a bitmap solution, but it does not work

--- day12.playground/Contents.swift
+++ day12.playground/Contents.swift
@@ -1,5 +1,4 @@
 import Foundation
-import PlaygroundSupport
 
 class PlantNode {
     var plant: PlantNode?
@@ -30,59 +29,72 @@ class PlantNode {
             for c in pattern {
                 curr = curr.add(c == "#")
             }
-            curr.add(result == "#")
+            _ = curr.add(result == "#")
             curr.rule = String(input)
         }
     }
 }
 
 class Garden {
-    var pots: [Bool]!
+    var pots: Int64 = 0
     var rules: PlantNode!
     var idxOffset = 0
     
     init(inputFile: String = "input") {
-        guard let filePath = Bundle.main.path(forResource: inputFile, ofType: "txt"),
-            let input = try? String(contentsOfFile: filePath) else {
-                print("Couldn't load file")
-                exit(1)
+        let input: String
+        if let filePath = Bundle.main.path(forResource: inputFile, ofType: "txt"),
+            let inp = try? String(contentsOfFile: filePath) {
+            input = inp
+        } else if let inp = try? String(contentsOfFile: inputFile) {
+            input = inp
+        } else {
+            print("Couldn't load file")
+            exit(1)
         }
         
         var lines = input.split(separator: "\n")
         let initialStateDefinition = lines.removeFirst().split(separator: ":")[1].trimmingCharacters(in: .whitespaces)
-        self.pots = initialStateDefinition.map({ $0 == "#" })
+        for (idx, c) in initialStateDefinition.enumerated() {
+            if c == "#" {
+                self.pots |= 1 << idx
+            }
+        }
         self.padPots()
-        print(lines.joined(separator: "\n"))
         self.rules = PlantNode(definitions: lines)
     }
     
     func padPots() {
-        if !self.pots.prefix(3).allSatisfy({ !$0 }) {
-            self.pots.insert(false, at: 0)
-            self.pots.insert(false, at: 0)
-            self.pots.insert(false, at: 0)
-            self.idxOffset += 3
+        while self.pots & 7 != 0 {
+            if self.pots & (7 << 62) != 0 {
+                print(self.description)
+                assert(false, "Must not shift up if top bits are taken")
+            }
+            
+            self.pots = self.pots << 1
+            self.idxOffset += 1
         }
-        if !self.pots.suffix(3).allSatisfy({ !$0 }) {
-            self.pots.append(false)
-            self.pots.append(false)
-            self.pots.append(false)
+        while self.pots & (7 << 62) != 0 {
+            if self.pots & 7 != 0 {
+                print(self.description)
+                assert(false, "Must not shift down if bottom bits are taken")
+            }
+                
+            self.pots = self.pots >> 1
+            self.idxOffset -= 1
         }
     }
     
     func incrementGeneration() {
-        var next: [Bool] = Array(self.pots)
-        for idx in 0..<pots.count-4 {
+        var next: Int64 = 0
+        for idx in 0..<60 {
             var curr = self.rules
-            for pot in self.pots[idx..<idx+5] {
-                curr = curr?.get(pot)
+            for b in idx..<idx+5 {
+                curr = curr?.get(self.pots & 1<<b != 0)
                 if curr == nil { break }
             }
-            if let result = curr {
+            if curr != nil {
                 //print("   \t" + Array(repeating: " ", count: idx).joined() + result.rule!)
-                next[idx+2] = result.empty == nil
-            } else {
-                next[idx+2] = false
+                next |= 1 << idx+2
             }
         }
         self.pots = next
@@ -91,8 +103,8 @@ class Garden {
     
     var sum: Int {
         var sum = 0
-        for idx in 0..<self.pots.count {
-            if self.pots[idx] {
+        for idx in 0..<64 {
+            if self.pots & Int64(1)<<idx != 0 {
                 sum += idx - self.idxOffset
             }
         }
@@ -100,19 +112,45 @@ class Garden {
     }
     
     var description: String {
-        return self.pots.map({ $0 ? "#" : "."}).joined() + "\tsum:\(self.sum)"
+        var s = ""
+        for idx in 0..<64 {
+            if self.pots & Int64(1)<<idx != 0 {
+                s.append("#")
+            } else {
+                s.append(".")
+            }
+        }
+        return "\(s)\tsum:\(self.sum)"
     }
 }
 
-let testgarden = Garden(inputFile: "test")
+if CommandLine.arguments.count > 0 && CommandLine.arguments.last?.suffix(3) == "txt" {
+    let arg = CommandLine.arguments.last!
+    let garden = Garden(inputFile: arg)
+    for _ in 0..<20 {
+        garden.incrementGeneration()
+    }
+    print("Sum of garden after 20 generations: \(garden.sum)")
+    
+    for i in 20..<50000000000 {
+        if i % 100000 == 0 { print(".") }
+        garden.incrementGeneration()
+    }
+    print("Sum of garden after lololol generations: \(garden.sum)")
+} else {
+    let testgarden = Garden(inputFile: "test")
 
-for generation in 0..<21 {
-    print("[\(generation)]\t\(testgarden.description)")
-    testgarden.incrementGeneration()
-}
+    for _ in 0..<20 {
+        testgarden.incrementGeneration()
+        print(testgarden.description)
+    }
 
-let garden = Garden()
-for generation in 0..<21 {
-    print("[\(generation)]\t\(garden.description)")
-    garden.incrementGeneration()
+    print("Sum of test garden: \(testgarden.sum)")
+
+    let garden = Garden()
+    for _ in 0..<20 {
+        garden.incrementGeneration()
+        print(testgarden.description)
+    }
+    print("Sum of garden after 20 generations: \(garden.sum)")
 }
