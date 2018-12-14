00a7824e4a463eb5ec4cb0ad9953bffc65fe9d8a
Ryan Forsythe <ryan@pdxcb.net>
2018-12-14 13:25:15 -0800

    28 star, thanks to KAO

--- day14.playground/Contents.swift
+++ day14.playground/Contents.swift
@@ -2,6 +2,17 @@ var recipes = [3,7]
 var e1idx = 0
 var e2idx = 1
 
+var recipelists = [[5,1,5,8,9], [0,1,2,4,5], [9,2,5,1,0], [5,9,4,1,4], [3,0,6,2,8,1]]
+
+func checkTargetRecipes() {
+    for (idx, recipe) in recipelists.enumerated() {
+        if Array(recipes.suffix(recipe.count)) == recipe {
+            print("Recipe list \(recipe) found at \(recipes.count - recipe.count)")
+            recipelists.remove(at: idx)
+        }
+    }
+}
+
 func appendRecipes() {
     let newvalue = recipes[e1idx] + recipes[e2idx]
     let tens: Int
@@ -14,8 +25,10 @@ func appendRecipes() {
     assert(ones < 10)
     if tens > 0 {
         recipes.append(tens)
+        checkTargetRecipes()
     }
     recipes.append(ones)
+    checkTargetRecipes()
 }
 
 func updateElves() {
@@ -36,3 +49,8 @@ assert(recipes[18..<28] == [9,2,5,1,0,7,1,0,8,5])
 // After 2018 recipes, the scores of the next ten would be 5941429882.
 assert(recipes[2018..<2028] == [5,9,4,1,4,2,9,8,8,2])
 print(recipes[306281..<306291])
+
+while recipelists.count > 0 {
+    appendRecipes()
+    updateElves()
+}
