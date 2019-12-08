2a954df3c9beaeada16b4153ddbab4c7bbf6e554
Ryan Forsythe <ryan@pdxcb.net>
2019-12-08 13:24:03 -0800

    New run call to parse input into a common format, then hand that parsed input to each of the parts.

--- 2019/Sources/AOC/main.swift
+++ 2019/Sources/AOC/main.swift
@@ -44,8 +44,16 @@ func run(_ day: String, _ p1: () -> Void, _ p2: () -> Void) -> Void {
     print("Part 2 completed in \(time(p2))s")
 }
 
+func run<A>(_ day: String, _ parse: (String) -> A, _ p1: (A) -> Void, _ p2: (A) -> Void) -> Void {
+    print("Day \(day)")
+    let inputFileName = "day\(day).txt"
+    var parsedInput: A?
+    print("Input file parsed in \(time({ parsedInput = parse(inputFileName) }))s")
+    print("Part 1 completed in \(time({ p1(parsedInput!) }))")
+    print("Part 1 completed in \(time({ p2(parsedInput!) }))")
+}
+
 switch day {
-/// To start a new day, copy and paste this case, updating to day x
 case "day1": run("1", day1.part1, day1.part2)
 case "day2": run("2", day2.part1, day2.part2)
 case "day3": run("3", day3.part1, day3.part2)
@@ -53,7 +61,7 @@ case "day4": run("4", day4.part1, day4.part2)
 case "day5": run("5", day5.part1, day5.part2)
 case "day6": run("6", day6.part1, day6.part2)
 case "day7": run("7", day7.part1, day7.part2)
-case "day8": run("8", day8.part1, day8.part2)
+case "day8": run("8", day8.parse, day8.part1, day8.part2)
 
 default:
     preconditionFailure("Unknown day \(day)")
--- 2019/Sources/day8/day8.swift
+++ 2019/Sources/day8/day8.swift
@@ -2,12 +2,13 @@ import AOCShared
 import Foundation
 import Overture
 
-public func part1() { print("Part 1: \(getMagicNumber(ingestFile("day8.txt").first!))") }
-public func part2() { print("Part 2: \n\(printableSIF(ingestFile("day8.txt").first!))") }
+public func parse(_ fileName: String) -> [[Int]] { layers(ingestFile(fileName).first!) }
+public func part1(_ layers: [[Int]]) { print("Part 1: \(getMagicNumber(layers))") }
+public func part2(_ layers: [[Int]]) { print("Part 2: \n\(printableSIF(layers))") }
 
-func getMagicNumber(_ s: String.SubSequence) -> Int {
+func getMagicNumber(_ layers: [[Int]]) -> Int {
     ///To make sure the image wasn't corrupted during transmission, the Elves would like you to find the layer that contains the fewest 0 digits. On that layer, what is the number of 1 digits multiplied by the number of 2 digits?
-    let layer = layers(s)
+    let layer = layers
         .map { (a: [Int]) -> [Int] in a.filter { $0 != 0 } }
         .map { (a: [Int]) -> (Int, Int, Int) in (a.count, a.filter({ $0 == 1 }).count, a.filter({ $0 == 2 }).count) }
         .sorted { a, b -> Bool in a.0 > b.0 }
@@ -22,8 +23,8 @@ func layers(_ s: String.SubSequence) -> [[Int]] {
         .map { (a: Int) -> [Int] in Array(input[a..<(a + (6 * 25))]) }
 }
 
-func printableSIF(_ s: String.SubSequence) -> String {
-    layers(s)
+func printableSIF(_ layers: [[Int]]) -> String {
+    layers
         .reduce(Array(repeating: Array(repeating: 2, count: 25), count: 6), integrateLayer)
         .map(renderSIFRows)
         .joined(separator: "\n")
