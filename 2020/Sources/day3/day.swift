3bf23a4104f8fbe785e2e266b2b9c04f26e68870
Ryan Forsythe <ryan@pdxcb.net>
2020-12-03 11:25:19 -0800

    Can't stop fucking around with day 3

--- 2020/Sources/day3/day.swift
+++ 2020/Sources/day3/day.swift
@@ -7,27 +7,25 @@ public func parse(_ input: String) -> [[Bool]] {
         .map { $0.map { $0 == "#" } }
 }
 
-func countTrees(_ pattern: [[Bool]], slope: (x: Int, y: Int)) -> Int {
+func countTrees(_ pattern: [[Bool]], slope: Point) -> Int {
     guard let w = pattern.first?.count else { fatalError("No first line in pattern!") }
-    var x = 0
-    var y = 1
     return pattern
-        .filter { line in
-            y -= 1
-            guard y == 0 else { return false }
-            let v = line[x % w]
-            x += slope.x
-            y += slope.y
-            return v
+        .reduce((sum: 0, position: Point(0, 1))) { accum, line in
+            var sum = accum.sum
+            var position = accum.position
+            position.y -= 1
+            guard position.y == 0 else { return (sum, position) }
+            if line[position.x % w] { sum += 1 }
+            return (sum, position + slope)
         }
-        .count
+        .sum
 }
 
 func multiSlope(_ pattern: [[Bool]]) -> Int {
     [(1,1), (3,1), (5,1), (7,1), (1,2)]
-        .map(curry(countTrees)(pattern))
+        .map(pipe(Point.init, curry(countTrees)(pattern)))
         .reduce(1, *)
 }
 
-public func part1(_ parsedInput: [[Bool]]) { print("Part 1: \(countTrees(parsedInput, slope: (3,1)))") }
+public func part1(_ parsedInput: [[Bool]]) { print("Part 1: \(countTrees(parsedInput, slope: Point(3,1)))") }
 public func part2(_ parsedInput: [[Bool]]) { print("Part 2: \(multiSlope(parsedInput))") }
