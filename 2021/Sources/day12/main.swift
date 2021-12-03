5091681ed256b6f0bdf0f411aa275c6552656eb1
Ryan Forsythe <ryan@pdxcb.net>
2021-12-02 16:30:25 -0800

    Update template

--- 2021/Sources/day12/main.swift
+++ 2021/Sources/day12/main.swift
@@ -1,17 +1,20 @@
 import AOCShared
 import Foundation
 import Overture
+import Parsing
 
 guard let inputFile = Bundle.module.url(forResource: "input", withExtension: "txt"),
       let input = try? String(contentsOf: inputFile) else {
     fatalError("Could not get contents of input file")
 }
 
-run(input: input, parse, part1, part2)
-
 public typealias ParsedStructure = Int
 
-public func parse(_ input: String) -> ParsedStructure { 1 }
+let parser: AnyParser<Substring, ParsedStructure> = Int.parser().eraseToAnyParser()
+run(input: input, parser, part1, part2)
+// or
+//public func parse(_ input: String) -> ParsedStructure { 1 }
+//run(input: input, parse, part1, part2)
+
 public func part1(_ parsedInput: ParsedStructure) { print("Part 1: ") }
 public func part2(_ parsedInput: ParsedStructure) { print("Part 2: ") }
-
