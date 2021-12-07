bf7c769ba57b6dd0dfafee7682847a3e7c359715
Ryan Forsythe <ryan@pdxcb.net>
2021-12-06 21:46:13 -0800

    day 7, slooooow style

--- 2021/Sources/day7/main.swift
+++ 2021/Sources/day7/main.swift
@@ -8,13 +8,64 @@ guard let inputFile = Bundle.module.url(forResource: "input", withExtension: "tx
     fatalError("Could not get contents of input file")
 }
 
-public typealias ParsedStructure = Int
+public typealias ParsedStructure = [Int]
 
-let parser: AnyParser<Substring, ParsedStructure> = Int.parser().eraseToAnyParser()
+let parser: AnyParser<Substring, ParsedStructure> = Many(Int.parser(), separator: ",").eraseToAnyParser()
 run(input: input, parser, part1, part2)
-// or
-//public func parse(_ input: String) -> ParsedStructure { 1 }
-//run(input: input, parse, part1, part2)
 
-public func part1(_ parsedInput: ParsedStructure) { print("Part 1: ") }
-public func part2(_ parsedInput: ParsedStructure) { print("Part 2: ") }
+func p1(_ parsedInput: ParsedStructure) -> Int {
+    (0...parsedInput.max()!)
+        .map { p in
+            parsedInput.reduce(0) { a, v in a + abs(v - p) }
+        }
+        .min()!
+}
+
+public func part1(_ parsedInput: ParsedStructure) {
+    let a = p1(parsedInput)
+    assert(a != 371, "previous guess")
+    print("Part 1: \(a)")
+}
+
+func p2(_ parsedInput: ParsedStructure) -> Int {
+    var costMemo: [Int] = [0]
+    func p2cost(_ v: Int) -> Int {
+        // don't blow the stack with recursion, no tco here
+        var curr = costMemo.count - 1
+        while curr < v {
+            let thisV = costMemo[curr]
+            costMemo.append(thisV + curr + 1)
+            curr += 1
+        }
+        
+        return costMemo[v]
+    }
+    
+    return (0...parsedInput.max()!)
+        .map { p in
+            parsedInput.reduce(0) { a, v in a + p2cost(abs(p - v)) }
+        }
+        .min()!
+}
+
+public func part2(_ parsedInput: ParsedStructure) {
+    let a = p2(parsedInput)
+    if a == 341558 { print("P2 WRONG ANSWER") }
+    print("Part 2: \(a)")
+}
+
+func testp1() {
+    let a = [16,1,2,0,4,2,7,1,2,14]
+    let answer = p1(a)
+    assert(answer == 37)
+}
+
+testp1()
+
+func testp2() {
+    let a = [16,1,2,0,4,2,7,1,2,14]
+    let answer = p2(a)
+    assert(answer == 168)
+}
+
+testp2()
