0b718fbfd796ccfb4b5eeb3e01e06ed9990381db
Ryan Forsythe <ryan@pdxcb.net>
2021-12-10 20:18:26 -0800

    day 9 & 10

--- 2021/Sources/day10/main.swift
+++ 2021/Sources/day10/main.swift
@@ -1,20 +1,169 @@
 import AOCShared
 import Foundation
 import Overture
-import Parsing
+//import Parsing
 
 guard let inputFile = Bundle.module.url(forResource: "input", withExtension: "txt"),
       let input = try? String(contentsOf: inputFile) else {
     fatalError("Could not get contents of input file")
 }
 
-public typealias ParsedStructure = Int
+public typealias ParsedStructure = [Substring]
 
-let parser: AnyParser<Substring, ParsedStructure> = Int.parser().eraseToAnyParser()
-run(input: input, parser, part1, part2)
-// or
-//public func parse(_ input: String) -> ParsedStructure { 1 }
-//run(input: input, parse, part1, part2)
+public func parse(_ input: String) -> ParsedStructure { input.split(separator: "\n") }
 
-public func part1(_ parsedInput: ParsedStructure) { print("Part 1: ") }
-public func part2(_ parsedInput: ParsedStructure) { print("Part 2: ") }
+run(input: input, parse, part1, part2)
+
+enum ParseError: Error {
+    case unexpected(Character)
+}
+
+func takeAndExpect(_ c: Character, _ input: inout String) -> Result<String, ParseError> {
+    _ = input.removeFirst()
+    let r = parenParser(&input)
+    switch r {
+    case .failure: return r
+    case .success(let completion):
+        guard let next = input.first else {
+            return .success(completion + [c])
+        }
+        if next == c {
+            _ = input.removeFirst()
+        } else {
+            return .failure(.unexpected(next))
+        }
+    }
+    
+    return parenParser(&input)
+}
+
+func parenParser(_ input: inout String) -> Result<String, ParseError> {
+    guard let peek = input.first else { return .success("") }
+    switch peek {
+    case "(": return takeAndExpect(")", &input)
+    case "[": return takeAndExpect("]", &input)
+    case "{": return takeAndExpect("}", &input)
+    case "<": return takeAndExpect(">", &input)
+    default: return .success("")
+    }
+}
+
+func scoreP1(_ cs: [Character]) -> Int {
+    cs.reduce(0) { accum, c in
+        switch c {
+        case ")": return accum + 3
+        case "]": return accum + 57
+        case "}": return accum + 1197
+        case ">": return accum + 25137
+        default: fatalError()
+        }
+    }
+}
+
+func p2Map(_ c: Character) -> Int {
+    switch c {
+    case ")": return 1
+    case "]": return 2
+    case "}": return 3
+    case ">": return 4
+    default: fatalError()
+    }
+}
+
+func scoresP2(_ completions: [String]) -> [Int] {
+    completions
+        .map { s in
+            s.reduce(0) { accum, c in
+                (accum * 5) + p2Map(c)
+            }
+        }
+        .sorted()
+}
+
+func scoreP2(_ completions: [String]) -> Int {
+    scoresP2(completions)[completions.count / 2]
+}
+
+public func part1(_ parsedInput: ParsedStructure) {
+    let bads = parsedInput.compactMap { line -> Character? in
+        var line = String(line)
+        switch parenParser(&line) {
+        case .failure(.unexpected(let c)): return c
+        case .success: return nil
+        }
+    }
+    let a = scoreP1(bads)
+    
+    print("Part 1: \(a)")
+}
+
+public func part2(_ parsedInput: ParsedStructure) {
+    let completed = parsedInput.compactMap { line -> String? in
+        var completedLine = String(line)
+        switch parenParser(&completedLine) {
+        case .failure: return nil
+        case .success(let completion): return completion
+        }
+    }
+    let score = scoreP2(completed)
+    print("Part 2: \(score)")
+}
+
+func testp1() {
+    let input = """
+[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]
+""".split(separator: "\n")
+    let expectation: [Character?] = [nil, nil, "}", nil, ")", "]", nil, nil, ">"]
+    zip(input, expectation)
+        .forEach { i, e in
+            guard e != nil else { return }
+            var s = String(i)
+            switch parenParser(&s) {
+            case .success: return
+            case .failure(.unexpected(let bad)):
+                assert(bad == e, "while parsing \(i): Expected \(e ?? "X"), got \(bad ?? "X"), unparsed \(s)")
+            }
+        }
+}
+
+testp1()
+
+func testp2() {
+    let input = """
+[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+(((({<>}<{<{<>}{[]{[]{}
+{<[[]]>}<{[{[{[]{()[[[]
+<{([{{}}[<[[[<>{}]]]>[]]
+""".split(separator: "\n")
+    let expectation = """
+}}]])})]
+)}>]})
+}}>}>))))
+]]}}]}]}>
+])}>
+""".split(separator: "\n")
+    let c = zip(input, expectation)
+        .map { i, e -> String in
+            var s = String(i)
+            switch parenParser(&s) {
+            case .success(let completion):
+                assert(completion == e, "while parsing \(i): Expected \(e), got \(completion), unparsed \(s)")
+                return completion
+            case .failure:
+                fatalError()
+            }
+        }
+    assert(scoreP2(c) == 288957)
+}
+
+testp2()
--- 2021/Sources/day9/main.swift
+++ 2021/Sources/day9/main.swift
@@ -8,13 +8,84 @@ guard let inputFile = Bundle.module.url(forResource: "input", withExtension: "tx
     fatalError("Could not get contents of input file")
 }
 
-public typealias ParsedStructure = Int
+public typealias ParsedStructure = [[Int]]
 
-let parser: AnyParser<Substring, ParsedStructure> = Int.parser().eraseToAnyParser()
-run(input: input, parser, part1, part2)
-// or
-//public func parse(_ input: String) -> ParsedStructure { 1 }
-//run(input: input, parse, part1, part2)
+public func parse(_ input: String) -> ParsedStructure {
+    input.split(separator: "\n").map { s in s.map { c in pipe(String.init, Int.init)(c)! } }
+}
 
-public func part1(_ parsedInput: ParsedStructure) { print("Part 1: ") }
-public func part2(_ parsedInput: ParsedStructure) { print("Part 2: ") }
+run(input: input, parse, part1, part2)
+
+func neighbors(_ p: Point, maxX: Int, maxY: Int) -> [Point] {
+    var out: [Point] = []
+    if p.y > 0 { out.append(Point(p.x, p.y - 1)) }
+    if p.x < maxX { out.append(Point(p.x + 1, p.y)) }
+    if p.y < maxY { out.append(Point(p.x, p.y + 1)) }
+    if p.x > 0 { out.append(Point(p.x - 1, p.y)) }
+    return out
+}
+
+func p1(_ input: ParsedStructure) -> Int {
+    let maxX = input.first!.count - 1
+    let maxY = input.count - 1
+    return input
+        .enumerated()
+        .map { y, row in
+            row
+                .enumerated()
+                .filter { x, val in
+                    neighbors(Point(x, y), maxX: maxX, maxY: maxY)
+                        .filter { input[$0.y][$0.x] <= val }
+                        .count == 0
+                }
+        }
+        .joined()
+        .reduce(0) { accum, t in accum + t.element + 1}
+}
+
+public func part1(_ parsedInput: ParsedStructure) {
+    let a = p1(parsedInput)
+    print("Part 1: \(a)")
+}
+
+func viz(_ input: ParsedStructure) {
+    print(input.map { $0.map(String.init).joined() }.joined(separator: "\n"))
+}
+
+func p2(_ input: ParsedStructure) -> Int {
+    let maxX = input.first!.count - 1
+    let maxY = input.count - 1
+    var all = Set((0..<input.first!.count).map { x in (0..<input.count).map { y in Point(x, y) } }.joined())
+    var binId = 10
+    var input = input
+    while all.count > 0 {
+        var toVisit = [all.removeFirst()]
+        while toVisit.count > 0 {
+            let curr = toVisit.removeFirst()
+            if input[curr.y][curr.x] == 9 { continue }
+            input[curr.y][curr.x] = binId
+            let unseenNeighbors = neighbors(curr, maxX: maxX, maxY: maxY).filter(all.contains)
+            unseenNeighbors.forEach { _ = all.remove($0) }
+            toVisit.append(contentsOf: unseenNeighbors)
+        }
+        binId += 1
+    }
+    
+    return input
+        .reduce(into: [Int: Int]()) { accum, row in
+            accum = row.reduce(into: accum) { accum, v in accum[v] = (accum[v] ?? 0) + 1 }
+        }
+        .filter { k, _ in k != 9 }
+        .values
+        .sorted()
+        .suffix(3)
+        .reduce(1, *)
+}
+
+public func part2(_ parsedInput: ParsedStructure) {
+    let a = p2(parsedInput)
+    if a <= 897156 { print("Wrong answer") }
+    if a >= 30303936 { print("Wrong answer") }
+    print("Part 2: \(a)")
+}
+
