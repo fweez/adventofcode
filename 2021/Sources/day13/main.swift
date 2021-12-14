4624407ecb036f2ae110d7ee42bfd58c7d10ddc9
Ryan Forsythe <ryan@pdxcb.net>
2021-12-14 15:23:40 -0800

    day 13 & 14

--- 2021/Sources/day13/main.swift
+++ 2021/Sources/day13/main.swift
@@ -8,13 +8,92 @@ guard let inputFile = Bundle.module.url(forResource: "input", withExtension: "tx
     fatalError("Could not get contents of input file")
 }
 
-public typealias ParsedStructure = Int
+enum Fold {
+    case x(Int)
+    case y(Int)
+}
 
-let parser: AnyParser<Substring, ParsedStructure> = Int.parser().eraseToAnyParser()
+typealias ParsedStructure = (Set<Point>, [Fold])
+
+let point = Int.parser()
+    .skip(",")
+    .take(Int.parser())
+    .map { Point($0, $1) }
+let points = Many(point, separator: "\n")
+    .map { Set($0) }
+let fold = OneOfMany(
+    StartsWith("fold along x").map { "x" },
+    StartsWith("fold along y").map { "y" })
+    .skip("=")
+    .take(Int.parser())
+    .map { s, v -> Fold in
+        switch s {
+        case "x": return .x(v)
+        case "y": return .y(v)
+        default: fatalError()
+        }
+    }
+let folds = Many(fold, separator: "\n")
+let parser: AnyParser<Substring, ParsedStructure> = points
+    .skip("\n\n")
+    .take(folds)
+    .eraseToAnyParser()
 run(input: input, parser, part1, part2)
 // or
 //public func parse(_ input: String) -> ParsedStructure { 1 }
 //run(input: input, parse, part1, part2)
 
-public func part1(_ parsedInput: ParsedStructure) { print("Part 1: ") }
-public func part2(_ parsedInput: ParsedStructure) { print("Part 2: ") }
+func doFold(_ fold: Fold, input: Set<Point>) -> Set<Point> {
+    input.reduce(into: Set<Point>()) { accum, p in
+        switch fold {
+        case .x(let foldPoint):
+            if p.x < foldPoint {
+                accum.insert(p)
+            } else {
+                let new = Point(foldPoint - (p.x - foldPoint), p.y)
+                accum.insert(new)
+            }
+        case .y(let foldPoint):
+            if p.y < foldPoint { accum.insert(p) }
+            else {
+                let new = Point(p.x, foldPoint - (p.y - foldPoint))
+                accum.insert(new)
+            }
+        }
+    }
+}
+
+func part1(_ parsedInput: ParsedStructure) {
+    print("\(parsedInput.0.count) points")
+    print("\(parsedInput.1.count) folds")
+    
+    let fold = parsedInput.1.first!
+    let ps = doFold(fold, input: parsedInput.0)
+    let a = ps.count
+    print("Part 1: \(a)")
+    
+}
+
+func viz(_ input: Set<Point>) -> String {
+    (0...8).map { y in
+        (0...42).map { x in
+            let curr = Point(x, y)
+            if input.contains(curr) {
+                return "#"
+            } else {
+                return "."
+            }
+        }
+        .joined()
+    }
+    .joined(separator: "\n")
+}
+
+func part2(_ parsedInput: ParsedStructure) {
+    let final = parsedInput.1.reduce(parsedInput.0) { input, fold in
+        doFold(fold, input: input)
+    }
+    print(viz(final))
+    print("Part 2: n/a")
+    
+}
--- 2021/Sources/day14/main.swift
+++ 2021/Sources/day14/main.swift
@@ -2,19 +2,119 @@ import AOCShared
 import Foundation
 import Overture
 import Parsing
+import Algorithms
 
 guard let inputFile = Bundle.module.url(forResource: "input", withExtension: "txt"),
       let input = try? String(contentsOf: inputFile) else {
     fatalError("Could not get contents of input file")
 }
 
-public typealias ParsedStructure = Int
+typealias ParsedStructure = (Substring, [String: Substring])
 
-let parser: AnyParser<Substring, ParsedStructure> = Int.parser().eraseToAnyParser()
+let template = CharacterSet.uppercaseLetters.eraseToAnyParser()
+let insertion = CharacterSet.uppercaseLetters
+    .skip(" -> ")
+    .take(CharacterSet.uppercaseLetters)
+    .map { (String($0), $1) }
+let insertions = Many(insertion, separator: "\n")
+    .map { Dictionary<String, Substring>(uniqueKeysWithValues: $0) }
+let parser: AnyParser<Substring, ParsedStructure> = template
+    .skip("\n\n")
+    .take(insertions)
+    .eraseToAnyParser()
+
 run(input: input, parser, part1, part2)
-// or
-//public func parse(_ input: String) -> ParsedStructure { 1 }
-//run(input: input, parse, part1, part2)
 
-public func part1(_ parsedInput: ParsedStructure) { print("Part 1: ") }
-public func part2(_ parsedInput: ParsedStructure) { print("Part 2: ") }
+func step(input: String, insertions: [String: Substring]) -> String {
+    input.reduce("") { accum, c in
+        guard let l = accum.last else { return accum + "\(c)" }
+        let k = "\(l)\(c)"
+        if let i = insertions[k] {
+            return accum + "\(i)\(c)"
+        } else {
+            return accum + "\(c)"
+        }
+    }
+}
+
+func runInsertions(
+    step: Int,
+    pair: String,
+    insertions: [String: Substring],
+    memo: inout [Int: [String: [Character: Int]]]
+) -> [Character: Int] {
+    let left = pair.first!
+    let right = pair.last!
+    guard step > 0,
+          let insertion = insertions[pair]?.first
+    else {
+        // Base case -- ran out of steps or this pair doesn't have an insertion
+        return [:]
+    }
+    if let memoized = memo[step]?[pair] {
+        return memoized
+    }
+    var leftCounts = runInsertions(
+        step: step - 1,
+        pair: "\(left)\(insertion)",
+        insertions: insertions,
+        memo: &memo)
+    let rightCounts = runInsertions(
+        step: step - 1,
+        pair: "\(insertion)\(right)",
+        insertions: insertions,
+        memo: &memo)
+    leftCounts.merge(rightCounts, uniquingKeysWith: +)
+    leftCounts[insertion] = (leftCounts[insertion] ?? 0) + 1
+    if memo[step] == nil {
+        memo[step] = [:]
+    }
+    memo[step]![pair] = leftCounts
+//    print("step \(step) pair \(pair) inserted \(insertion): \(leftCounts)")
+    return leftCounts
+}
+
+func origPart1(_ parsedInput: ParsedStructure) {
+    let polymer = (0..<10)
+        .reduce(String(parsedInput.0)) { accum, _ in
+            return step(input: accum, insertions: parsedInput.1)
+        }
+    let d = parsedInput.1.values
+        .reduce(into: [:] as [String: Int]) { accum, c in
+            let s = String(c)
+            accum[s] = polymer.filter { String($0) == s }.count
+        }
+    print(d)
+    let s = d.values.sorted()
+    let a = s.last! - s.first!
+    print("Part 1: \(a)")
+}
+
+func runit(_ parsedInput: ParsedStructure, steps: Int) -> Int {
+    var memo: [Int: [String: [Character: Int]]] = [:]
+    var initial: [Character: Int] = [:]
+    parsedInput.0.forEach { c in
+        initial[c] = (initial[c] ?? 0) + 1
+    }
+    let counts = parsedInput.0
+        .adjacentPairs()
+        .reduce(into: initial) { accum, pair in
+            let p = "\(pair.0)\(pair.1)"
+            let new = runInsertions(step: steps, pair: p, insertions: parsedInput.1, memo: &memo)
+            accum.merge(new, uniquingKeysWith: +)
+        }
+    let vs = counts.values.sorted()
+    return vs.last! - vs.first!
+}
+
+func part1(_ parsedInput: ParsedStructure) {
+    let a = runit(parsedInput, steps: 10)
+    if a != 2375 { print("You fucked it up") }
+    print("Part 1: \(a)")
+}
+
+func part2(_ parsedInput: ParsedStructure) {
+    let a = runit(parsedInput, steps: 40)
+    print("Part 2: \(a)")
+
+}
